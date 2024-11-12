e9a72de8072744bb6bb50025da629e151374c438
Vedant Dhepe <vedantdhepe101@gmail.com>
2024-11-12 13:46:49 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+            // Typed javascript
+            var typed = new Typed('#element', {
+              strings: ['Build', 'Grow', 'Lead'],
+              typeSpeed: 100,
+            });
+
+            
+            
